ed88ec9fc2ee764e716c434750544a61c9a46ede
michielgrotenhuis <michiel@cmy.nl>
2025-05-08 09:23:41 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/getting-started/what-is-ecommerce">
-            Walkthrough - 5min ⏱️
+            <translate>Walkthrough - 5min ⏱️</translate>
           </Link>
         </div>
       </div>
