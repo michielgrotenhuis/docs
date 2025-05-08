93e0b00ffac5fa984fd6e270bf3f763b1e9f4258
michielgrotenhuis <michiel@cmy.nl>
2025-05-08 14:48:22 +0200

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import styles from './styles.module.css';
+import Translate, {translate} from '@docusaurus/Translate';
 
 const SectionList = [
   {
